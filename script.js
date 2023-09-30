1a40efd908e360217ba70fba43a30c6e2d27b6ba
Yaqutatin <143370825+Yaqutatin@users.noreply.github.com>
2023-09-30 19:09:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-querySelector('button').addEventListener('click', function() {
+// script.js
+
+// Mengubah nama profil ketika tombol "Edit Profile" ditekan
+document.querySelector('.edit-profile').addEventListener('click', function() {
     document.querySelector('h2').textContent = "Jane Doe";
+});
+
+// Mengirim pesan ketika tombol "Message" ditekan
+document.querySelector('.message').addEventListener('click', function() {
+    alert("Pesan berhasil terkirim!");
+});
+
+// Menampilkan komentar ketika gambar postingan diklik
+document.querySelectorAll('.post img').forEach(function(image) {
+    image.addEventListener('click', function() {
+        var post = this.closest('.post');
+        var comment = post.querySelector('.comment');
+        comment.style.display = 'block';
+    });
 });
